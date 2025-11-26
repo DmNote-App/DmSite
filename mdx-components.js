65993d1e981397f1776112c029245a9479769e0d
lee-sihun <cnsa201119@gmail.com>
2025-11-26 11:15:25 +0900

    feat: Nextra 4 호환 Card 컴포넌트 추가 및 MDX에서 전역 사용 가능하도록 수정

--- mdx-components.js
+++ mdx-components.js
@@ -1,12 +1,25 @@
-import { useMDXComponents as getThemeComponents } from "nextra-theme-docs"; // nextra-theme-blog or your custom theme
+import { useMDXComponents as getThemeComponents } from "nextra-theme-docs";
+import { Callout, Cards, Tabs, Steps, FileTree } from "nextra/components";
 
 // Get the default MDX components
 const themeComponents = getThemeComponents();
+
+// Nextra 컴포넌트들을 전역으로 제공 (import 없이 MDX에서 사용 가능)
+const nextraComponents = {
+  Callout,
+  Cards,
+  Card: Cards.Card, // Cards.Card를 Card로 별칭
+  Tabs,
+  Tab: Tabs.Tab,
+  Steps,
+  FileTree,
+};
 
 // Merge components
 export function useMDXComponents(components) {
   return {
     ...themeComponents,
+    ...nextraComponents,
     ...components,
   };
 }
