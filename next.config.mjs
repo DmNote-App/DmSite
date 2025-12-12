5d98dd8458392507def43008cc56de0caa8ff313
연우 <imyeonu@pm.me>
2025-12-13 00:39:19 +0900

    깃허브 액션으로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -7,5 +7,9 @@ const withNextra = nextra({
 
 // Export the final Next.js config with Nextra included
 export default withNextra({
-  // ... Add regular Next.js options here
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 });
