8f46bc774c1f3f500f7203bd1ab20bb30249abe6
lee-sihun <cnsa201119@gmail.com>
2025-12-22 13:48:17 +0900

    feat: EditLinkFixer 컴포넌트 추가 및 레이아웃에 통합

--- app/docs/layout.jsx
+++ app/docs/layout.jsx
@@ -3,6 +3,7 @@ import { Banner, Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import "nextra-theme-docs/style.css";
 import { docsMetadata } from "../seo.config";
+import EditLinkFixer from "./components/EditLink";
 
 export const metadata = docsMetadata;
 
@@ -11,7 +12,11 @@ const navbar = (
   <Navbar
     logo={
       <div className="flex items-center gap-2">
-        <img src="/icon.ico" alt="DM Note Logo" className="w-6 h-6 rounded-md" />
+        <img
+          src="/icon.ico"
+          alt="DM Note Logo"
+          className="w-6 h-6 rounded-md"
+        />
         <b>DM Note</b>
       </div>
     }
@@ -27,12 +32,14 @@ const footer = (
 export default async function DocsLayout({ children }) {
   return (
     <Layout
-      banner={banner}
+      // banner={banner}
       navbar={navbar}
       pageMap={await getPageMap()}
-      docsRepositoryBase="https://github.com/DmNote-App/DmNote"
+      docsRepositoryBase="https://github.com/DmNote-App/DmNote/tree/master/docs/plugin"
+      editLink="Edit this page"
       footer={footer}
     >
+      <EditLinkFixer />
       {children}
     </Layout>
   );
